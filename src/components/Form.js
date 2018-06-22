b5196472d18abfafb705885b2cc6606315c77ea2
Stephen Engler <sengler1989@gmail.com>
2018-06-22 15:17:27 -0500

    add form component

---
+++ src/components/Form.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+const initalState = {
+
+}
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = initalState
+  }
+  render() {
+    return <p>this is a Form</p>;
+  }
+}
+
+export default Form;
